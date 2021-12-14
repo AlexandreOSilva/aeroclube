3a8cbfab62f7d9221380ab29f7d8536c475ee188
AlexandreOSilva <alexandreosilva@outlook.pt>
2021-12-14 10:42:16 +0000

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href={{asset("https://www.w3schools.com/w3css/4/w3.css")}}>
+<link rel="stylesheet" href={{asset("https://fonts.googleapis.com/css?family=Raleway")}}>
+<link rel="stylesheet" href={{asset("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")}}>
 <link rel="stylesheet" href={{asset('css/Home.css')}}>
 
 @extends('layout.master')
@@ -17,7 +17,7 @@
       <span class="w3-large">Stop wasting valuable time with projects that just isn't you.</span>
       <p><a href="#about" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p>
     </div>
-    <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
+    <div class="w3-display-bottomleft w3-text-white w3-large" style="padding:24px 48px">
       <i class="fa fa-facebook-official w3-hover-opacity"></i>
       <i class="fa fa-instagram w3-hover-opacity"></i>
       <i class="fa fa-snapchat w3-hover-opacity"></i>
